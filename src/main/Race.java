8429f8f3005f1eb3b36aae24d0fedc1cf2cd4f65
Mr.Chimnani <jaish.chimnani_cs19@gla.ac.in>
2020-10-29 16:26:01 +0530

    calculating results

--- src/main/Race.java
+++ src/main/Race.java
@@ -3,11 +3,29 @@ package main;
 import thread.rabbit.Rabbit;
 import thread.tortoise.Tortoise;
 
+import javax.swing.*;
+
 public class Race {
     public static void main(String[] args) {
         Rabbit rabbit = new Rabbit();
         rabbit.getRabbit().start();
         Tortoise tortoise = new Tortoise ();
         tortoise.getTortoise ().start ();
+        String winner;
+
+        while((rabbit.getRabbit ().isAlive () || tortoise.getTortoise ().isAlive ())){
+            if(tortoise.getTimeInSeconds () < rabbit.getTimeInSeconds ()){
+                winner = "TORTOISE";
+            }
+            else if(rabbit.getTimeInSeconds () == tortoise.getTimeInSeconds ()){
+                winner ="NOBODY";
+            }
+            else {
+                winner = "RABBIT";
+            }
+            System.out.println ("Rabbit has finished in " + rabbit.getTimeInSeconds ()+" sec ");
+            System.out.println ("Tortoise has finished in "+ tortoise.getTimeInSeconds ()+" sec ");
+            JOptionPane.showMessageDialog (null,"Winner of the race is "+winner,"RESULTS",1);
+        }
     }
 }
