dc695460f662f9dd96c78aaf932e1b27dc7b161e
Mr.Chimnani <jaish.chimnani_cs19@gla.ac.in>
2020-10-29 11:43:25 +0530

    creating constant variables and getter methods in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,25 @@
 package thread.tortoise;
+import java.lang.Runnable;
+public class Tortoise implements Runnable {
+    private static final byte TOTAL_NUMBER_OF_MILESTONES = (byte)5;
+    private final Thread tortoise;
 
-public class Tortoise {
+    private float timeInSeconds = 0f;
+
+    public float getTimeInSeconds(){
+        return timeInSeconds;
+    }
+
+    public Tortoise(){
+        this.tortoise = new Thread(this,"Tortoise");
+    }
+
+    public Thread getTortoise(){
+        return tortoise;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
